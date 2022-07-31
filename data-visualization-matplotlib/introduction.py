50a4210014dea6753c12bd0ce25a7d0562747a08
fairuz anatasya <Ihsanti.fairuz@gmail.com>
2022-08-01 00:15:29 +0700

    still working on loading the column ??

--- data-visualization-matplotlib/introduction.py
+++ data-visualization-matplotlib/introduction.py
@@ -1,12 +1,13 @@
 #%%
 # we are now importing the libraries and file required for this analysis
 from calendar import calendar
+from turtle import color
 import matplotlib.pyplot as plt
 import numpy as py
 import pandas as pd
 import calendar
 
-seattle_weather = pd.read_csv("seattle_weather.csv")
+seattle_weather = pd.read_csv("seattle_weather_edited.csv")
 austin_weather = pd.read_csv("austin_weather.csv")
 
 
@@ -14,11 +15,10 @@ austin_weather = pd.read_csv("austin_weather.csv")
 # We need to modify the csv file a bit to suit it with the analysis
 
 # first, we need to select specific location for seattle
-seattle_weather.set_index("NAME", inplace=True)
-seattle_weather = seattle_weather.loc[['SEATTLE TACOMA INTERNATIONAL AIRPORT, WA US']]
+# seattle_weather.set_index("NAME", inplace=True)
+# seattle_weather = seattle_weather.loc[['SEATTLE TACOMA INTERNATIONAL AIRPORT, WA US']]
 
-
-# then, the modification about the month
+# then, turm the month-number into the month name
 seattle_weather = seattle_weather[['NAME', 'DATE', 'MLY-PRCP-NORMAL']]
 seattle_weather_month = seattle_weather["DATE"].astype(int)
 # seattle_weather_month = slice(seattle_weather_month)
@@ -28,9 +28,18 @@ austin_weather_month = austin_weather["DATE"].astype(int)
 
 
 #%%
-fig, ax = plt.subplots()
-ax.plot(seattle_weather_month, seattle_weather["MLY-PRCP-NORMAL"])
-ax.plot(austin_weather_month, austin_weather["MLY-PRCP-NORMAL"])
+fig, ax = plt.subplots(4, 1)
+
+ax[0].plot(seattle_weather_month, seattle_weather["MLY-PRCP-NORMAL"], color='b')
+# ax[0].plot(seattle_weather_month, seattle_weather["MLY-PRCP-75PCTL"], color='b')
+# ax[0].plot(seattle_weather_month, seattle_weather["MLY-PRCP-25PCTL"], color='b')
+ax[1].plot(seattle_weather_month, seattle_weather["MLY-HTDD-NORMAL"], color='b')
+ax[2].plot(austin_weather_month, austin_weather["MLY-PRCP-NORMAL"], color='r')
+# ax[1].plot(austin_weather_month, austin_weather["MLY-PRCP-75PCTL"], color='r')
+# ax[1].plot(austin_weather_month, austin_weather["MLY-PRCP-25PCTL"], color='r')
+ax[3].plot(austin_weather_month, austin_weather["MLY-HTDD-NORMAL"], color='r')
+
+# the "MLY-HTDD-NORMAL" cant be executed :(
 
 plt.show()
 
